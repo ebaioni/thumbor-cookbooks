2879ad4f861d2498c70a84c1634b6bb866a65908
Enrico Baioni <enrico.baioni@zanui.com.au>
2013-11-18 10:12:44 +1100

    comments how to move everything under chef

--- zanui-thumbor/recipes/default.rb
+++ zanui-thumbor/recipes/default.rb
@@ -11,24 +11,29 @@ bash "install_thumbor" do
   cwd "/tmp"
   code <<-EOH
 
-  if [ -f .thumbor_installed ]
-  then
-      exit
-  fi
+    #specify resource and states and chef takes care of transiction from one state to another
 
   curl http://python-distribute.org/distribute_setup.py | sudo python
+  #do it with chef apt cookbook https://github.com/zanui/shop-box2/blob/master/zanui-cookbooks/zanui-web/recipes/php.rb
   sudo echo "deb http://ppa.launchpad.net/thumbor/ppa/ubuntu precise main" | sudo tee -a /etc/apt/sources.list > /dev/null
   sudo echo "deb-src http://ppa.launchpad.net/thumbor/ppa/ubuntu precise main" | sudo tee -a /etc/apt/sources.list > /dev/null
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C6C3D73D1225313B
   sudo aptitude update -yq
+  ####
+   ##
+# Install other php packages
+#['php5-dev', 'php-pear', 'php5-memcache'].each do |pkg|
+ #   package pkg
+#end
   sudo aptitude install -yq thumbor
 
   sudo aptitude install -yq redis-server
   sudo update-rc.d redis-server enable > /dev/null
 
-  THUMBOR_PORT=`cat host/etc/thumbor.port`
+  THUMBOR_PORT=`cat host/etc/thumbor.port` //nginx
   THUMBOR_INSTANCES=#{node['thumbor']['processes']}
 
+    #move it into template
   BASE_PORT=#{node['thumbor']['base_port']}
   PORTS=''
   SERVERS=''
@@ -39,13 +44,23 @@ bash "install_thumbor" do
   done
   sudo cp host/etc/thumbor.nginx /etc/nginx/conf.d/thumbor.conf
   sudo perl -pi -e "s/SERVER_STUB/$SERVERS/;s/PORT/$THUMBOR_PORT/" /etc/nginx/conf.d/thumbor.conf
+
+#look at the template here https://github.com/zanui/shop-box2/blob/master/zanui-cookbooks/zanui-web/recipes/nginx.rb
   sudo mv /etc/default/thumbor /etc/default/thumbor.orig
+#how to restart: define service and call it example https://github.com/zanui/shop-cookbooks/blob/master/nginx/recipes/default.rb
   sudo cp host/etc/thumbor.default /etc/default/thumbor
+
+
   echo "port="$PORTS | sudo tee -a /etc/default/thumbor > /dev/null
+
   sudo mv /etc/thumbor.conf /etc/thumbor.conf.orig
+
   sudo cp host/etc/thumbor.conf.default /etc/thumbor.conf
+
   cat host/etc/thumbor.conf.custom | sudo tee -a /etc/thumbor.conf > /dev/null
+#template with no source only content
   sudo mv /etc/thumbor.key /etc/thumbor.key.orig
+
   if [ -s host/etc/thumbor.key ]
   then
       sudo cp host/etc/thumbor.key /etc/thumbor.key
@@ -53,8 +68,6 @@ bash "install_thumbor" do
       < /dev/urandom tr -dc a-z0-9 | head -c16 | sudo tee /etc/thumbor.key > /dev/null
   fi
 
-  sudo rm /var/lib/dhcp/*
 
-  touch .thumbor_installed
   EOH
 end
