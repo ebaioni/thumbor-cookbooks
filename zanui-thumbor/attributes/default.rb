d278ae1a5170ed34060104730276e0db6ecd2428
ebaioni <baio88@gmail.com>
2013-11-17 19:31:37 +1100

    zanui-thumbor

---
+++ zanui-thumbor/attributes/default.rb
@@ -0,0 +1,7 @@
+default["thumbor"]["processes"] = 4
+default["thumbor"]["base_port"] = 9000
+default["thumbor"]["queue"]["type"] = 'redis'
+default["thumbor"]["storage"]["type"] = 'file'
+default["thumbor"]["options"] = {}
+default["thumbor"]["options"]["QUALITY"] = 85
+default["thumbor"]["options"]["SECURITY_KEY"] = ''
